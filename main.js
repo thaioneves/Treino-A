8f19bad0d7df27c7feb704eeec6fb217a2bf7170
thaioneves <thais.oneves2101@gmail.com>
2026-05-19 21:33:22 -0300

    adição

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const professor = {
     nome: 'Amanda Flora',
     idade: 32,
-    telefone: 12345-6789
-    diciplina: 'TI'
-    escola: 'Senac'
+    telefone: 12345-6789,
+    disciplina: 'TI',
+    escola: 'Senac',
 }
+
+console.log(
+    "Nome:" + professor.nome + "/n" +
+    "Idade:" + professor.idade + "anos /n" +
+    "Telefone:" + professor.telefone + "/n" +
+    "Disciplina:" + professor.desciplina + "/n" +
+    "Escola:" + professor.escola + "/n" +
+)
